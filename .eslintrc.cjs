63208536e14f896364c35cdda4b78b99bf997192
TDomagalski <tomekwebdev@gmail.com>
2022-10-27 16:07:21 +0200

    feat(Navigation) add Navigation component && fix(eslintrc.cjs) add important options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,11 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Navigation() {
+  return (
+    <header>
+      <div className="nav_logo">
+        {/* <img src={Logo} alt="TomDom" /> */}
+        {/* <LogoIcon /> */}
+        <h2>TomDom</h2>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Link1</a>
+          </li>
+          <li>
+            <a href="#">Link2</a>
+          </li>
+          <li>
+            <a href="#">Link3</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="nav_footer">
+        <h6>footer navigation</h6>
+      </div>
+    </header>
+  );
+}
