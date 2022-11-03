4ad416d07eebeb336e924c78d210096e615e3cbe
TDomagalski <tomekwebdev@gmail.com>
2022-11-04 00:46:44 +0100

    feat: add front account page

--- src/pages/PageAccount.jsx
+++ src/pages/PageAccount.jsx
@@ -3,41 +3,25 @@ import React from 'react';
 export default function PageAccount() {
   return (
     <>
-      <h1>Account</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-        repellendus dolor aliquam ad iure vel placeat nisi ducimus accusantium,
-        possimus, vitae dolorum nemo quidem. Reprehenderit quisquam perferendis
-        officia nostrum ullam! Minima, quas inventore provident officiis,
-        expedita soluta saepe consequuntur aperiam porro aliquid eius sapiente
-        ipsam iusto quasi quam? Neque dolores fuga dolorum, nulla fugit nesciunt
-        assumenda odio amet soluta consectetur. Modi rerum animi dolorum non
-        eaque possimus, iure veniam tenetur tempora quas autem corrupti placeat
-        ex quaerat distinctio numquam optio, totam minima, ut consequuntur dicta
-        laudantium est odio architecto! Voluptatibus. Pariatur eum at officiis,
-        asperiores esse inventore. Voluptatum alias nemo reprehenderit
-        repudiandae pariatur iusto ullam dolores neque asperiores. Reiciendis
-        impedit ipsum quibusdam sequi molestiae vero laudantium molestias,
-        delectus debitis unde. Sit quasi quisquam culpa nemo sapiente magni quas
-        harum illum consequuntur ex minus minima quia fugit fugiat quis aperiam,
-        impedit architecto non qui repellendus maxime modi, molestiae quaerat!
-        Adipisci, qui? Ipsum rerum repellendus reprehenderit assumenda dolorem
-        fuga quod labore facere, ullam ea vitae! Et dolorum dicta aperiam
-        impedit quae facere sunt cumque enim dolore, recusandae dolorem? Ratione
-        aliquam saepe eius. Neque amet ipsa minima, vitae nesciunt molestias
-        explicabo illo libero aliquid ut cumque ullam sequi vero rem earum.
-        Fugiat natus iusto aliquid libero optio numquam quaerat sed molestiae
-        magnam necessitatibus? Autem eius officiis vero necessitatibus
-        consequuntur dolores. Eaque asperiores quo similique in quis consequatur
-        voluptate soluta aliquid tenetur velit cumque deleniti ut assumenda
-        modi, commodi expedita ratione pariatur est doloremque. Atque expedita
-        amet maiores nam ipsam optio magni iusto mollitia nihil assumenda
-        excepturi eaque, velit sed maxime officiis corporis facilis sit quam et
-        ducimus quo, porro ipsa qui saepe. Magnam. Natus quidem ipsam ad itaque
-        eveniet. Maxime possimus ut voluptas iure ullam exercitationem dolores
-        suscipit assumenda. Dolores iusto aut animi ex, tempora neque iste
-        accusamus reprehenderit illo quas placeat laborum!
-      </p>
+      <h1>Register Account</h1>
+      <div className="form_container">
+        <form className="form_register">
+          <input
+            className="form_field"
+            placeholder="First Name"
+            name="firstName"
+          />
+          <input
+            className="form_field"
+            placeholder="Last Name"
+            name="lastName"
+          />
+          <input className="form_field" placeholder="Email" name="email" />
+          <button className="form_field" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
     </>
   );
 }
